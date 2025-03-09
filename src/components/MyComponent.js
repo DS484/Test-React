00b70ab5ff32be679aa4d67cfd47c7edf5ffb627
DucSang404 <ndsang404@gmail.com>
2025-03-09 17:01:55 +0700

    Components

---
+++ src/components/MyComponent.js
@@ -0,0 +1,17 @@
+// class component
+
+import React from 'react';
+
+class MyComponent extends React.Component {
+    // jsx
+    render() {
+        return (
+            <div>
+                Duc Sang\n
+                {Math.random()}
+            </div>
+        );
+    }
+}
+
+export default MyComponent;
